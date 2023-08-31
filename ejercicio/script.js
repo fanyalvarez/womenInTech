b20ab498c3929415920bb687321d6bca54557220
fanyalvarez <alvarezpestefania@outlook.com>
2023-08-30 22:25:09 -0600

    act config on links

---
+++ ejercicio/script.js
@@ -0,0 +1,21 @@
+const palabra1 = "hi"
+const palabra2 = "como"
+const palabra3 = "estas"
+
+const logica1 = palabra1.length > palabra2.length ? palabra1 : palabra2
+const logica2 = logica1.length > palabra3.length ? logica1 : palabra3
+
+console.log(logica2);
+
+const palabras = ['hi', 'como', 'estas']
+
+const mayor = ''
+
+for (let index = 0; index < palabras.length; index++) {
+
+    // mayor = palabras[index] < palabras[index + 1]
+
+}
+
+console.log(mayor);
+console.log(palabras);
